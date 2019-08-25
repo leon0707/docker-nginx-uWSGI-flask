f80c9f6408a547f19a46f7cc00fa3d562a398c24
Leon Feng <best.sum@gmail.com>
2019-08-24 21:25:44 -0400

    add nginx

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 def create_app():
     app = Flask(__name__)
 
     @app.route('/')
     def hello():
-        return '<h1>Hello There!</h1>'
+        return '<h1>Hello There! You IP is {}</h1>'.format(
+            request.environ.get('X-Real-IP', request.remote_addr))
 
     return app
 
